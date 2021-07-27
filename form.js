90577016e2ea76357295b3ea5ae2ae1d91109a55
Schaff <schaff.mathieu93@gmail.com>
2021-07-27 21:19:44 +0200

    add gitignore

--- form.js
+++
@@ -1,9 +0,0 @@
-const closeButton = document.getElementById('closeButton');
-const contactModal = document.querySelector('.contact-modal');
-const contactMe = document.querySelector('.contact-me');
-contactMe.addEventListener('click', () => {
-  contactModal.style.display = 'fixed';
-});
-closeButton.addEventListener('click', () => {
-  contactModal.style.display = 'none';
-});
