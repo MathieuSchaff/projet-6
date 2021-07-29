891222fd185156656ab3e816e2742c27c853d9c6
Schaff <schaff.mathieu93@gmail.com>
2021-07-29 20:54:02 +0200

    sort a continuer

--- fetch.js
+++ fetch.js
@@ -8,41 +8,35 @@ let myInit = {
   cache: 'default',
 };
 let myRequest = new Request('data.json', myInit);
-
-async function getRequest() {
-  let reponse = await fetch(myRequest);
-  let data = await reponse.json();
-  return data;
-}
 const mediaImage = [];
 const mediaVideo = [];
 let loaded = 0;
-let btns = [];
-let p;
-async function getPhotographer() {
+let arrayPhotographers;
+let array3;
+// recupère données
+const getRequest = async () => {
+  let reponse = await fetch(myRequest);
+  let data = await reponse.json();
+  return data;
+};
+// return une array photographer
+const getPhotographer = async () => {
   let { photographers } = await getRequest();
   loaded = 1;
-  p = Array.from(photographers);
+  arrayPhotographers = Array.from(photographers);
   return photographers;
-}
+};
 
-const parentChild = document.getElementById('articles');
-async function createPhotographer() {
-  let photographers;
-  if (!loaded) {
-    photographers = await getPhotographer();
-  } else {
-    photographers = await getSortedPhotographer();
-  }
-
-  console.log(photographers);
+const createPhotographer = async (method) => {
+  loaded = 1;
+  const articlesContainer = document.getElementById('articles');
+  let photographers = await method;
   photographers.forEach((element) => {
-    console.log(element);
     let articlePhotographer = document.createElement('article');
     articlePhotographer.classList.add('main');
     articlePhotographer.innerHTML = `
     <a href="#" class="main__link">
-      <img src="./FishEyePhotos/Sample Photos/Photographers ID Photos/${element.portrait}" alt="La moche" class="main__photo">
+      <img src="./FishEyePhotos/Sample Photos/Photographers ID Photos/${element.portrait}" alt="Photo portrait du photographe" class="main__photo">
       <h2 class="main__name">${element.name}</h2>
     </a>
     <p class="main__paragraph">
@@ -50,53 +44,107 @@ async function createPhotographer() {
       <p class="main__philosophy">${element.tagline}</p>
       <p class="main__tarif"> <span class="tarif">${element.price}</span>€/ jour</p>
     </p>`;
-    parentChild.appendChild(articlePhotographer);
+    articlesContainer.appendChild(articlePhotographer);
     let tagsDiv = document.createElement('div');
     tagsDiv.classList.add('tags');
     for (let i = 0; i < element.tags.length; i++) {
+      let btns = [];
       let tag = document.createElement('button');
       tag.setAttribute('type', 'button');
-      tag.innerHTML = `#${element.tags[i]}`;
+      tag.classList.add('nav--button');
+      tag.innerHTML = `#${element.tags[i]} <span class="sr-only">Tag</span>`;
       btns.push(tag);
       tagsDiv.appendChild(tag);
     }
     articlePhotographer.appendChild(tagsDiv);
   });
-}
-
-const buttons = document.querySelectorAll('button');
+};
+createPhotographer(getPhotographer());
+const buttons = document.querySelectorAll('.nav--button');
 const buttonsArr = Array.from(buttons);
 buttonsArr.forEach((e) =>
   e.addEventListener('click', () => {
     if (e.classList.contains('buttonClicked')) {
+      document.getElementById('articles').innerHTML = '';
+
+      console.log(sortedPhotographer());
       e.classList.remove('buttonClicked');
+      // createPhotographer(sortedPhotographer());
     } else {
+      document.getElementById('articles').innerHTML = '';
       e.classList.add('buttonClicked');
-      // p.photographers.sort(getSortedPhotographer(e.id));
+      console.log(sortedPhotographer());
+      // createPhotographer(sortedPhotographer());
     }
-    console.log(e.id);
   })
 );
-createPhotographer();
-// async function getSortedPhotographer(motFiltrant) {
-//   let photographers = await getPhotographer();
-//   for (let i = 0; i < photographers.length; i++) {
-//     if (photographers[i].tags.includes(motFiltrant)) {
+// quand je clique sur un bouton, si le bouton correspond a un tag alors cela incrémente le count de 1 sur l'article en question
+// doit return la nouvelle array crée à passer en
+const sortedPhotographer = async () => {
+  let { photographers } = await getRequest();
+  photographers.map((x) => (x.count = 0));
+
+  // photographers.map((x) => ({
+  //   x.count = 0;
+  //   if(x.tags.includes(e.id)){
+  //     x.count += 1;
+  //   }else {
+  //     x.count = 0;
+  //   }
+
+  // }))
+  // photographers.forEach((el) => {
+  //   if (el.tags.includes(e.id)) {
+  //     el.count = 0;
+  //     if (el.tags.includes(e.id)) {
+  //       el.count += 1;
+  //     } else {
+  //       el.count = 0;
+  //     }
+  //   }
+  // });
+  // pour tous les boutons coché, j'ajoute 1 a count
+  for (let i = 0; i < buttonsArr.length; i++) {
+    for (let i = 0; i < photographers.length; i++) {
+      // console.log(document.querySelector('.buttonClicked').id);
+      if (photographers[i].tags.includes(document.querySelector('.buttonClicked').id)) {
+        photographers[i].count += 1;
+      } else if (
+        !photographers[i].tags.includes(document.querySelector('.buttonClicked').id) &&
+        photographers[i].count > 0
+      ) {
+        photographers[i].count -= 1;
+      } else {
+        photographers[i].count = 0;
+      }
+    }
+  }
+
+  photographers.sort((a, b) => {
+    return b.count - a.count;
+  });
+  return console.log(photographers);
+  // return photographers;
+};
+// function getSortedPhotographer() {
+//   for (let i = 0; i < arrayPhotographers.length; i++) {
+//     if (arrayPhotographers[i].tags.includes('portrait')) {
 //       return 1;
 //     } else {
 //       return 0;
 //     }
 //   }
 // }
-// function compare(list, motFiltrant) {
-//   for (let i = 0; i < photographers.length; i++) {
-//     if (list.photographers.tags.includes(motFiltrant)) {
-//       return 1;
+// arrayPhotographers.sort(function getSortedPhotographer() {
+//   for (let i = 0; i < arrayPhotographers.length; i++) {
+//     if (arrayPhotographers[i].tags.includes('portrait')) {
+//       return console.log(arrayPhotographers[i] + arrayPhotographers[i].tags.includes('portrait'));
 //     } else {
-//       return 0;
+//       return console.log(arrayPhotographers[i] + arrayPhotographers[i].tags.includes('portrait'));
 //     }
 //   }
-// }
+// });
+
 // p.sort();
 // const search = function (list, search) {
 //   // if (
@@ -120,3 +168,23 @@ createPhotographer();
 //   return a.name.length - b.name.length;
 // };
 // console.log(p.sort(comparator));
+// function sort() {
+//   p.map((x) => {
+//     if (x.tags.includes('portrait')) {
+//       x.push('count');
+//     }
+//   });
+// }
+
+// for (let i = 0; i < arrayPhotographers.length; i++) {
+//   console.log(arrayPhotographers[i].tags);
+// }
+
+// let count = 'count';
+// function sort() {
+//   arrayPhotographers.forEach((element) => {
+//     if (element.tags.includes(buttonId)) {
+//       count += 1;
+//     }
+//   });
+// }
--- form.js
+++ form.js
@@ -1,9 +1,39 @@
 const closeButton = document.getElementById('closeButton');
-const contactModal = document.querySelector('.contact-modal');
+let modal = null;
 const contactMe = document.querySelector('.contact-me');
 contactMe.addEventListener('click', () => {
   contactModal.style.display = 'fixed';
 });
 closeButton.addEventListener('click', () => {
   contactModal.style.display = 'none';
+});
+const openModal = function (e) {
+  e.preventDefault();
+  modal = document.querySelector('.contact-modal');
+  modal.style.display = null;
+  modal.removeAttribute('aria-hidden');
+  modal.setAttribute('aria-modal', true);
+  closeButton.addEventListener('click', closeModal);
+  modal.addEventListener('click', stopPropagation);
+};
+const closeModal = function (e) {
+  e.preventDefault();
+  modal.style.display = 'none';
+  modal.setAttribute('aria-hidden', true);
+  modal.removeAttribute('aria-modal');
+  closeButton.removeEventListener('click', closeModal);
+  modal.removeEventListener('click', stopPropagation);
+  modal == null;
+};
+contactMe.addEventListener('click', openModal);
+const stopPropagation = function(e){
+  e.stopPropagation();
+}
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' || e.key === 'Esc') {
+    closeModal(e);
+  }
+  if (e.key === 'Tab ' && ) {
+
+  }
 });
