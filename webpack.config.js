1eb2396d8a15ad2fba742390ebb5b9b5f672202e
Schaff <schaff.mathieu93@gmail.com>
2021-11-15 03:24:44 +0100

    config webpack + build done

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,41 @@
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'production',
+//   entry: {
+//     app: './index.js',
+//   },
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'public'),
+//     },
+//     compress: true,
+//     port: 9000,
+//   },
+//   modules: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: [],
+//       },
+//     ],
+//   },
+// };
+
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    app: './index.js',
+    app: "./index.js"
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 9000,
-  },
-  modules: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [],
-      },
-    ],
-  },
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
